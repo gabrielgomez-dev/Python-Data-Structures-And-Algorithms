68cb2596b99a7f372daeda640aa9d5fd3063b0c3
Gabriel Gomez <pc866794@gmail.com>
2024-05-16 23:23:31 -0400

    pop and append are added

--- 1_LinkedLists/linked_lists.py
+++ 1_LinkedLists/linked_lists.py
@@ -16,8 +16,9 @@ class LinkedList:
             self.tail = new_node
             self.length = 1
 
-    def printList(self):
+    def print_list(self):
         current = self.head
+
         while current is not None:
             print(current.value)
             current = current.next
@@ -32,18 +33,58 @@ class LinkedList:
 
         self.tail = new_node
         self.length += 1
+        return True
+
+    def pop(self):
+        value = None
+
+        if self.length == 0:
+            return value
+
+        if self.length == 1:
+            value = self.head.value
+            self.head = None
+            self.tail = None
+        else:
+            current = self.head
+            while current.next.next:
+                current = current.next
+
+            value = current.next.value
+            current.next = None
+            self.tail = current
+
+        self.length -= 1
+        return value
+
+    def prepend(self, value):
+        if self.length == 0:
+            self.append(value)
+        else:
+            new_node = Node(value)
+            new_node.next = self.head
+            self.head = new_node
+            self.length += 1
+            return True
 
 
+# Code for test
 my_linked_list = LinkedList()
 
-my_linked_list.append(4)
+""" my_linked_list.append(4)
 my_linked_list.append(5)
 my_linked_list.append(6)
-my_linked_list.append(7)
+my_linked_list.append(7) """
 
-print("Head:", my_linked_list.head.value)
-print("Tail:", my_linked_list.tail.value)
+my_linked_list.pop()
+my_linked_list.pop()
+
+my_linked_list.prepend(3)
+my_linked_list.prepend(2)
+my_linked_list.prepend(1)
+
+my_linked_list.print_list()
+
 print("Length:", my_linked_list.length)
-
-print("\nPrinting list ...")
-my_linked_list.printList()
+# print("Head:", my_linked_list.head.value)
+# print("Tail:", my_linked_list.tail.value)
